bdc30962e7b6304786aa3e730b6695f660bee8d3
Naan Vasconcelos <naan.vasconcelos@hotmail.com>
2024-07-16 12:00:00 -0300

    Adicao de funcionarios e modificacao main

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
 import tkinter as tk
 from tkinter import ttk
-from tela.tela_inicial import TelaInicial
+from visao.tela.tela_login import TelaLogin
 
-class Login:
+class MainApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("Login")
-        self.abrir_tela_inicial()
-        '''
-        self.frame = tk.Frame(self.root, bg="#F0DAAE")
-        self.frame.pack(expand=True, fill="both")
-
-        self.botao_login = ttk.Button(self.frame, text="Login", style="Estilo.TButton", command=self.abrir_tela_inicial)
-        self.botao_login.pack(pady=20)
-        '''
-    def abrir_tela_inicial(self):
         self.root.withdraw()
-        self.tela_inicial = TelaInicial(self.root)
+        self.root.title("Sistema de Gestão")
+        self.tela_login = TelaLogin(self.root)
 
 if __name__ == "__main__":
     root = tk.Tk()
-    login = Login(root)
+    app = MainApp(root)
     root.mainloop()
