303a066297e7a10f96265057129600f5d544d8f8
Naan Vasconcelos <naan.vasconcelos@hotmail.com>
2024-07-17 12:01:00 -0300

    Modificao de lugar e arquivo buscar cadastrar autor

--- visao/tela/autor/buscar.py
+++ visao/tela/autor/buscar.py
@@ -1,22 +1,23 @@
 import tkinter as tk
 from tkinter import ttk
-from tela.tela_padrao import TelaPadrao
+from visao.tela.tela_padrao import TelaPadrao
 from controle.autor_controller import AutorController
 
 class TelaBuscarAutor(TelaPadrao):
     def __init__(self, root, voltar_callback):
         super().__init__(root, "Buscar Autor")
+        self.janela.focus_set()
         self.voltar_callback = voltar_callback
         self.autor_controller = AutorController()
 
-        label_titulo = tk.Label(self.frame_central, text="Buscar Autor", font=("Montserrat", 18, "bold"), fg="#482E1D", bg="#F0DAAE")
+        label_titulo = tk.Label(self.frame_central, text="Buscar Autor", font=("Montserrat", 18, "bold"), fg="#893F04", bg="#E5E0D8")
         label_titulo.grid(row=0, column=0, columnspan=2, pady=20)
 
-        tk.Label(self.frame_central, text="Nome:", font=("Montserrat", 10), fg="#482E1D", bg="#F0DAAE").grid(row=1, column=0, pady=5, sticky='e', padx=15)
+        tk.Label(self.frame_central, text="Nome:", font=("Montserrat", 10), fg="#893F04", bg="#E5E0D8").grid(row=1, column=0, pady=5, sticky='e', padx=15)
         self.entry_buscar_nome = tk.Entry(self.frame_central)
         self.entry_buscar_nome.grid(row=1, column=1, pady=5)
 
-        self.label_mensagem = tk.Label(self.frame_central, text="", font=("Montserrat", 8), fg="#482E1D", bg="#F0DAAE")
+        self.label_mensagem = tk.Label(self.frame_central, text="", font=("Montserrat", 8), fg="#893F04", bg="#E5E0D8")
         self.label_mensagem.grid(row=2, column=0, columnspan=2, pady=10)
 
         botao_buscar = ttk.Button(self.frame_central, text="Buscar", style="Estilo.TButton", command=self.buscar_autor)
--- visao/tela/autor/cadastro.py
+++ visao/tela/autor/cadastro.py
@@ -1,30 +1,31 @@
 import tkinter as tk
 from tkinter import ttk
-from tela.tela_padrao import TelaPadrao
+from visao.tela.tela_padrao import TelaPadrao
 from controle.autor_controller import AutorController
 
 class TelaCadastroAutor(TelaPadrao):
     def __init__(self, root, voltar_callback):
         super().__init__(root, "Cadastrar Autor")
+        self.janela.focus_set()
         self.voltar_callback = voltar_callback
         self.autor_controller = AutorController()
 
-        label_titulo = tk.Label(self.frame_central, text="Cadastrar Autor", font=("Montserrat", 16, "bold"), fg="#482E1D", bg="#F0DAAE")
+        label_titulo = tk.Label(self.frame_central, text="Cadastrar Autor", font=("Montserrat", 16, "bold"), fg="#893F04", bg="#E5E0D8")
         label_titulo.grid(row=0, column=0, columnspan=2, pady=20)
 
-        tk.Label(self.frame_central, text="Nome:", font=("Montserrat", 10), fg="#482E1D", bg="#F0DAAE").grid(row=1, column=0, pady=5, sticky='e', padx=15)
+        tk.Label(self.frame_central, text="Nome:", font=("Montserrat", 10), fg="#893F04", bg="#E5E0D8").grid(row=1, column=0, pady=5, sticky='e')
         self.entry_nome = tk.Entry(self.frame_central)
         self.entry_nome.grid(row=1, column=1, pady=5)
 
-        tk.Label(self.frame_central, text="Nacionalidade:", font=("Montserrat", 10), fg="#482E1D", bg="#F0DAAE").grid(row=2, column=0, pady=5, sticky='e')
+        tk.Label(self.frame_central, text="Nacionalidade:", font=("Montserrat", 10), fg="#893F04", bg="#E5E0D8").grid(row=2, column=0, pady=5, sticky='e')
         self.entry_nacionalidade = tk.Entry(self.frame_central)
         self.entry_nacionalidade.grid(row=2, column=1, pady=5)
 
-        tk.Label(self.frame_central, text="Data de Nascimento:", font=("Montserrat", 10), fg="#482E1D", bg="#F0DAAE").grid(row=3, column=0, pady=5, sticky='e')
+        tk.Label(self.frame_central, text="Data de Nascimento:", font=("Montserrat", 10), fg="#893F04", bg="#E5E0D8").grid(row=3, column=0, pady=5, sticky='e')
         self.entry_data_nascimento = tk.Entry(self.frame_central)
         self.entry_data_nascimento.grid(row=3, column=1, pady=5)
 
-        self.label_mensagem = tk.Label(self.frame_central, text="", font=("Montserrat", 8), fg="#482E1D", bg="#F0DAAE")
+        self.label_mensagem = tk.Label(self.frame_central, text="", font=("Montserrat", 8), fg="#893F04", bg="#E5E0D8")
         self.label_mensagem.grid(row=4, column=0, columnspan=2, pady=10)
 
         botao_salvar = ttk.Button(self.frame_central, text="Salvar", style="Estilo.TButton", command=self.cadastrar_autor)
