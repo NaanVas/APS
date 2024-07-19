c55d45620c752ebd40dddb9a593f929920da317e
Naan Vasconcelos <naan.vasconcelos@hotmail.com>
2024-07-19 12:02:00 -0300

    Modificao de lugar e arquivo de listar e excluir funcionario

--- visao/tela/funcionario/excluir.py
+++ visao/tela/funcionario/excluir.py
@@ -1,22 +1,23 @@
 import tkinter as tk
 from tkinter import ttk
-from tela.tela_padrao import TelaPadrao
+from visao.tela.tela_padrao import TelaPadrao
 from controle.funcionario_controller import FuncionarioController
 
 class TelaExcluirFuncionario(TelaPadrao):
     def __init__(self, root, voltar_callback):
         super().__init__(root, "Excluir Funcionário")
+        self.janela.focus_set()
         self.voltar_callback = voltar_callback
         self.funcionario_controller = FuncionarioController()
 
-        label_titulo = tk.Label(self.frame_central, text="Excluir Funcionário", font=("Montserrat", 18, "bold"), fg="#482E1D", bg="#F0DAAE")
+        label_titulo = tk.Label(self.frame_central, text="Excluir Funcionário", font=("Montserrat", 18, "bold"), fg="#893F04", bg="#E5E0D8")
         label_titulo.grid(row=0, column=0, columnspan=2, pady=20)
 
-        tk.Label(self.frame_central, text="CPF:", font=("Montserrat", 10), fg="#482E1D", bg="#F0DAAE").grid(row=1, column=0, pady=5, sticky='e')
+        tk.Label(self.frame_central, text="CPF:", font=("Montserrat", 10), fg="#893F04", bg="#E5E0D8").grid(row=1, column=0, pady=5, sticky='e')
         self.entry_cpf = tk.Entry(self.frame_central)
         self.entry_cpf.grid(row=1, column=1, pady=5)
 
-        self.label_mensagem = tk.Label(self.frame_central, text="", font=("Montserrat", 8), fg="#482E1D", bg="#F0DAAE")
+        self.label_mensagem = tk.Label(self.frame_central, text="", font=("Montserrat", 8), fg="#893F04", bg="#E5E0D8")
         self.label_mensagem.grid(row=2, column=0, columnspan=2, pady=10)
 
         self.botao_excluir = ttk.Button(self.frame_central, text="Excluir", style="Estilo.TButton", command=self.excluir_funcionario)
--- visao/tela/funcionario/listar.py
+++ visao/tela/funcionario/listar.py
@@ -1,27 +1,29 @@
 import tkinter as tk
-from tkinter import ttk
-from tela.tela_padrao import TelaPadrao
+from visao.tela.tela_padrao import TelaPadrao
 from controle.funcionario_controller import FuncionarioController
 
 class TelaListarFuncionarios(TelaPadrao):
     def __init__(self, root, voltar_callback):
         super().__init__(root, "Listagem de Funcionários")
+        self.janela.focus_set()
         self.voltar_callback = voltar_callback
         self.funcionario_controller = FuncionarioController()
         self.text_area = None  
+
+        self.janela.geometry("600x450+800+150")
 
         self.configurar_interface()
 
     def configurar_interface(self):
         self.janela_listar_aberta = True
 
-        label_titulo = tk.Label(self.frame_central, text="Listagem de Funcionários", font=("Montserrat", 18, "bold"), fg="#482E1D", bg="#F0DAAE")
+        label_titulo = tk.Label(self.frame_central, text="Listagem de Funcionários", font=("Montserrat", 18, "bold"), fg="#893F04", bg="#E5E0D8")
         label_titulo.grid(row=0, column=0, columnspan=2, pady=20)
 
         scrollbar = tk.Scrollbar(self.janela)
         scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-        self.text_area = tk.Text(self.janela, wrap=tk.WORD, yscrollcommand=scrollbar.set, bg="#F0DAAE", font=("Montserrat", 10))
+        self.text_area = tk.Text(self.janela, wrap=tk.WORD, yscrollcommand=scrollbar.set, bg="#E5E0D8", font=("Montserrat", 10))
         self.text_area.pack(fill=tk.BOTH, expand=True)
 
         scrollbar.config(command=self.text_area.yview)
@@ -54,6 +56,4 @@ class TelaListarFuncionarios(TelaPadrao):
                 self.text_area.insert(tk.END, data_nascimento)
                 self.text_area.insert(tk.END, "\n")
 
-    def voltar(self):
-        self.janela.destroy()
-        self.voltar_callback()
+                self.text_area.tag_configure("negrito", font=("Montserrat", 10, "bold"))
