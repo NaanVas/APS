d727e0660607ec6b32edcd74d9c2fd20995aa2a8
Naan Vasconcelos <naan.vasconcelos@hotmail.com>
2024-08-19 12:00:00 -0300

    Modificando modelo

--- modelo/emprestimo.py
+++ modelo/emprestimo.py
@@ -1,12 +1,11 @@
-from datetime import datetime
-
 class Emprestimo:
-    def __init__(self, cpf_funcionario, cpf_usuario):
+    def __init__(self, cpf_funcionario, cpf_usuario, status=True):
         self.cpf_funcionario = cpf_funcionario
         self.cpf_usuario = cpf_usuario
         self.livros = []
         self.data_emprestimo = None
         self.data_devolucao = None
+        self.status = status
 
     def get_cpf_funcionario(self):
         return self.cpf_funcionario
@@ -37,3 +36,9 @@ class Emprestimo:
 
     def set_data_devolucao(self, data_devolucao):
         self.data_devolucao = data_devolucao
+
+    def get_status(self):
+        return self.status
+
+    def set_status(self, status):
+        self.status = status
