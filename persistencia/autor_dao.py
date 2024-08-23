b76f6af3452ff2534915c5bd94c54e40ea9a4456
Naan Vasconcelos <naan.vasconcelos@hotmail.com>
2024-08-23 12:00:00 -0300

    Modificacao de autor_dao para herdar a base

--- persistencia/autor_dao.py
+++ persistencia/autor_dao.py
@@ -1,16 +1,14 @@
-import csv
 from modelo.autor import Autor
+from persistencia.dao_base import DAOBase
 
-class AutorDAO:
-    def __init__(self):
-        self.arquivo_csv_autores = 'autores.csv'
+class AutorDAO(DAOBase):
+    def __init__(self, arquivo_csv_autores='autores.csv'):
+        super().__init__(arquivo_csv_autores)
 
     def salvar_autor(self, autor):
-        with open(self.arquivo_csv_autores, mode='a', newline='') as file:
-            writer = csv.writer(file)
-            if file.tell() == 0:
-                writer.writerow(['Nome', 'Nacionalidade', 'Data de Nascimento'])
-            writer.writerow([autor.get_nome(), autor.get_nacionalidade(), autor.get_data_nascimento()])
+        with self._abrir_arquivo(modo='a') as file:
+            self._escrever_cabecalho(file, ['Nome', 'Nacionalidade', 'Data de Nascimento'])
+            self._escrever_linha(file, [autor.get_nome(), autor.get_nacionalidade(), autor.get_data_nascimento()])
 
         print(f'Autor {autor.get_nome()} salvo com sucesso no arquivo CSV.')
 
@@ -18,28 +16,22 @@ class AutorDAO:
         autores = self.listar_autores()
         autores_filtrados = [autor for autor in autores if autor.get_nome() != nome]
         self._salvar_todos_autores(autores_filtrados)
-        print(f"Autor '{nome}' excluido com sucesso.")
+        print(f"Autor '{nome}' excluído com sucesso.")
 
     def buscar_autor(self, nome):
-        with open(self.arquivo_csv_autores, mode='r', newline='') as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                if row['Nome'] == nome:
-                    return Autor(row['Nome'], row['Nacionalidade'], row['Data de Nascimento'])
+        for row in self._ler_arquivo():
+            if row['Nome'] == nome:
+                return Autor(row['Nome'], row['Nacionalidade'], row['Data de Nascimento'])
         return None
 
     def listar_autores(self):
         autores = []
-        with open(self.arquivo_csv_autores, mode='r', newline='') as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                autor = Autor(row['Nome'], row['Nacionalidade'], row['Data de Nascimento'])
-                autores.append(autor)
+        for row in self._ler_arquivo():
+            autor = Autor(row['Nome'], row['Nacionalidade'], row['Data de Nascimento'])
+            autores.append(autor)
         return autores
 
     def _salvar_todos_autores(self, autores):
-        with open(self.arquivo_csv_autores, mode='w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(['Nome', 'Nacionalidade', 'Data de Nascimento'])
-            for autor in autores:
-                writer.writerow([autor.get_nome(), autor.get_nacionalidade(), autor.get_data_nascimento()])
+        cabecalho = ['Nome', 'Nacionalidade', 'Data de Nascimento']
+        dados = [[autor.get_nome(), autor.get_nacionalidade(), autor.get_data_nascimento()] for autor in autores]
+        self._salvar_todos(cabecalho, dados)
