af78e9931b40bd0f5623d7cf91fc6bd50acb39c0
Naan Vasconcelos <naan.vasconcelos@hotmail.com>
2024-08-26 12:00:00 -0300

    Modificacao de emprestimo_dao para herdar a base

--- persistencia/emprestimo_dao.py
+++ persistencia/emprestimo_dao.py
@@ -1,14 +1,13 @@
-import csv
 from datetime import datetime
 from modelo.emprestimo import Emprestimo
 from controle.livro_controller import LivroController
+from persistencia.dao_base import DAOBase
 
-class EmprestimoDAO:
+class EmprestimoDAO(DAOBase):
     def __init__(self, arquivo='emprestimos.csv', arquivo_devolucoes='devolucoes.csv'):
-        self.arquivo = arquivo
+        super().__init__(arquivo)
         self.arquivo_devolucoes = arquivo_devolucoes
         self.livro_controller = LivroController()
-
 
     def criar_emprestimo(self, cpf_funcionario, cpf_usuario):
         return Emprestimo(cpf_funcionario, cpf_usuario)
@@ -39,13 +38,11 @@ class EmprestimoDAO:
         data_devolucao = (emprestimo.get_data_devolucao().strftime('%Y-%m-%d')
                           if emprestimo.get_data_devolucao() else '')
 
-        with open(self.arquivo, mode='a', newline='', encoding='utf-8') as file:
-            writer = csv.writer(file)
-            if file.tell() == 0:
-                writer.writerow([
-                    'CPF Funcionário', 'CPF Usuário', 'Livros', 'Data Empréstimo', 'Data Devolução', 'Status'
-                ])
-            writer.writerow([
+        with self._abrir_arquivo(modo='a') as file:
+            self._escrever_cabecalho(file, [
+                'CPF Funcionário', 'CPF Usuário', 'Livros', 'Data Empréstimo', 'Data Devolução', 'Status'
+            ])
+            self._escrever_linha(file, [
                 emprestimo.get_cpf_funcionario(),
                 emprestimo.get_cpf_usuario(),
                 ', '.join([livro.get_titulo() for livro in emprestimo.get_livros()]),
@@ -54,30 +51,25 @@ class EmprestimoDAO:
                 emprestimo.get_status()
             ])
 
-    def listar_emprestimos(self, todos=None):
+    def listar_emprestimos(self,):
         emprestimos = []
-        with open(self.arquivo, mode='r', newline='', encoding='utf-8') as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                cpf_funcionario = row['CPF Funcionário']
-                cpf_usuario = row['CPF Usuário']
-                livros = row['Livros']
-                data_emprestimo = row['Data Empréstimo']
-                data_devolucao = row['Data Devolução']
-                status = row['Status'] == 'True'
+        for row in self._ler_arquivo():
+            cpf_funcionario = row['CPF Funcionário']
+            cpf_usuario = row['CPF Usuário']
+            livros = row['Livros']
+            data_emprestimo = row['Data Empréstimo']
+            data_devolucao = row['Data Devolução']
+            status = row['Status']
 
-                livros_lista = livros.split(', ') if livros else []
+            livros_lista = livros.split(', ') if livros else []
 
-                emprestimo = Emprestimo(cpf_funcionario, cpf_usuario, status)
-                emprestimo.set_livros(livros_lista)
-                emprestimo.set_data_emprestimo(datetime.strptime(data_emprestimo, '%Y-%m-%d %H:%M:%S') if data_emprestimo else None)
-                emprestimo.set_data_devolucao(datetime.strptime(data_devolucao, '%Y-%m-%d') if data_devolucao else None)
-                if todos == None:
-                    if status:
-                        emprestimos.append(emprestimo)
-                        return emprestimos
-                
-                emprestimos.append(emprestimo)
+            emprestimo = Emprestimo(cpf_funcionario, cpf_usuario, status)
+            emprestimo.set_livros(livros_lista)
+            emprestimo.set_data_emprestimo(datetime.strptime(data_emprestimo, '%Y-%m-%d %H:%M:%S') if data_emprestimo else None)
+            emprestimo.set_data_devolucao(datetime.strptime(data_devolucao, '%Y-%m-%d') if data_devolucao else None)
+            emprestimo.set_status(status)
+
+            emprestimos.append(emprestimo)
 
         return emprestimos
 
@@ -85,7 +77,7 @@ class EmprestimoDAO:
         emprestimos = self.listar_emprestimos()
         
         for emprestimo in emprestimos:
-            if emprestimo.get_cpf_usuario() == cpf and emprestimo.get_status():
+            if emprestimo.get_cpf_usuario() == cpf and emprestimo.get_status() == True:
                 return f"O usuário '{cpf}' possui empréstimos em aberto."
         return None
     
@@ -109,16 +101,15 @@ class EmprestimoDAO:
             for emprest in emprestimos:
                 if emprest.get_cpf_usuario() == emprestimo.get_cpf_usuario():
                     emprest.set_status(False)
-            #emprestimos =  self.listar_emprestimos("todos")
             self._salvar_todos_emprestimos(emprestimos)
+        
+        self.set_arquivo(self.arquivo_devolucoes)
 
-        with open(self.arquivo_devolucoes, mode='a', newline='', encoding='utf-8') as file:
-            writer = csv.writer(file)
-            if file.tell() == 0:
-                writer.writerow([
-                    'CPF Funcionário', 'CPF Usuário', 'Título Livro', 'Data Devolução', 'Data Devolvida', 'Multa'
-                ])
-            writer.writerow([
+        with self._abrir_arquivo(modo='a') as file:
+            self._escrever_cabecalho(file, [
+                'CPF Funcionário', 'CPF Usuário', 'Título Livro', 'Data Devolução', 'Data Devolvida', 'Multa'
+            ])
+            self._escrever_linha(file, [
                 emprestimo.get_cpf_funcionario(),
                 emprestimo.get_cpf_usuario(),
                 li.get_titulo(),
@@ -126,21 +117,22 @@ class EmprestimoDAO:
                 data_devolvida.strftime('%Y-%m-%d'),
                 multa if multa is not None else ''
             ])
+        
+        self.restaurar_arquivo_original()
 
     def _salvar_todos_emprestimos(self, emprestimos):
-        with open(self.arquivo, mode='w', newline='', encoding='utf-8') as file:
-            writer = csv.writer(file)
-        
-            writer.writerow([
-                'CPF Funcionário', 'CPF Usuário', 'Livros', 'Data Empréstimo', 'Data Devolução', 'Status'
-            ])
-        
-            for emprestimo in emprestimos:
-                writer.writerow([
-                    emprestimo.get_cpf_funcionario(),
-                    emprestimo.get_cpf_usuario(),
-                    ', '.join([livro for livro in emprestimo.get_livros()]),
-                    emprestimo.get_data_emprestimo().strftime('%Y-%m-%d %H:%M:%S') if emprestimo.get_data_emprestimo() else '',
-                    emprestimo.get_data_devolucao().strftime('%Y-%m-%d') if emprestimo.get_data_devolucao() else '',
-                    'True' if emprestimo.get_status() else 'False'
-                ])
+        cabecalho = [
+            'CPF Funcionário', 'CPF Usuário', 'Livros', 'Data Empréstimo', 'Data Devolução', 'Status'
+        ]
+        dados = [
+            [
+                emprestimo.get_cpf_funcionario(),
+                emprestimo.get_cpf_usuario(),
+                ', '.join([livro for livro in emprestimo.get_livros()]),
+                emprestimo.get_data_emprestimo().strftime('%Y-%m-%d %H:%M:%S') if emprestimo.get_data_emprestimo() else '',
+                emprestimo.get_data_devolucao().strftime('%Y-%m-%d') if emprestimo.get_data_devolucao() else '',
+                'True' if emprestimo.get_status() else 'False'
+            ]
+            for emprestimo in emprestimos
+        ]
+        self._salvar_todos(cabecalho, dados)
